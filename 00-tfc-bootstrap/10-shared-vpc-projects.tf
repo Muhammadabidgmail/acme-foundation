6ab2fb084fcf6de2389a25f4342da8a4cce070fa
Abid <ask4abid@gmail.com>
2023-10-27 00:39:04 +0500

    Update 10-shared-vpc-projects.tf

--- 00-tfc-bootstrap/10-shared-vpc-projects.tf
+++ 00-tfc-bootstrap/10-shared-vpc-projects.tf
@@ -54,7 +54,7 @@ resource "tfe_workspace" "shared-vpc-project-stg" {
   ]
   vcs_repo {
     identifier     = var.github_repo
-    oauth_token_id = data.tfe_oauth_client.cclient.oauth_token_id
+    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
   }
 }
 
