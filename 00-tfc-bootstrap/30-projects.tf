12b24cc0c16423f07248ffcac358746ff5730581
Abid <ask4abid@gmail.com>
2023-10-27 00:16:50 +0500

    Update 30-projects.tf

--- 00-tfc-bootstrap/30-projects.tf
+++ 00-tfc-bootstrap/30-projects.tf
@@ -15,7 +15,7 @@ resource "tfe_workspace" "projects-dev" {
   ]
   vcs_repo {
     identifier     = var.github_repo
-    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
+    oauth_token_id = data.tfe_oauth_client.cclient.oauth_token_id
   }
 }
 
@@ -93,7 +93,7 @@ resource "tfe_workspace" "projects-prd" {
   ]
   vcs_repo {
     identifier     = var.github_repo
-    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
+    oauth_token_id = data.tfe_oauth_client.cclient.oauth_token_id
   }
 }
 
