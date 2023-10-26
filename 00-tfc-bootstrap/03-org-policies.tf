86c016006dc54ec029ef8d3e9ba5c9deb1f1f7ca
Abid <ask4abid@gmail.com>
2023-10-27 00:20:56 +0500

    Update 03-org-policies.tf

--- 00-tfc-bootstrap/03-org-policies.tf
+++ 00-tfc-bootstrap/03-org-policies.tf
@@ -12,7 +12,7 @@ resource "tfe_workspace" "org-policies" {
   working_directory             = "03-org-policies"
   vcs_repo {
     identifier     = var.github_repo
-    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
+    oauth_token_id = data.tfe_oauth_client.cclient.oauth_token_id
   }
 }
 
