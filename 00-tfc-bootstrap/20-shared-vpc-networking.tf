ee0a0b3dd034c743cbd6ad54b94f183f89449d24
Abid <ask4abid@gmail.com>
2023-10-27 00:24:24 +0500

    Update 20-shared-vpc-networking.tf

--- 00-tfc-bootstrap/20-shared-vpc-networking.tf
+++ 00-tfc-bootstrap/20-shared-vpc-networking.tf
@@ -49,7 +49,7 @@ resource "tfe_workspace" "shared-vpc-networking-stg" {
   ]
   vcs_repo {
     identifier     = var.github_repo
-    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
+    oauth_token_id = data.tfe_oauth_client.cclient.oauth_token_id
   }
 }
 
